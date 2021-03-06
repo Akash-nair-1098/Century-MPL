b3529f056e2ffac5497e26d7847d6209c6c1f5ec
DhriShyaM <akashappu1098@gmail.com>
2021-03-06 16:10:09 +0530

    centurys mpl

---
+++ home.js
@@ -0,0 +1,16 @@
+var insta=document.querySelector("#instablack");
+insta.addEventListener("mouseover",()=>{
+    insta.src="logos/insta.png"
+})
+insta.addEventListener("mouseout",()=>{
+    insta.src="logos/instablack.png"
+})
+
+
+var fb=document.querySelector("#fbblack");
+fb.addEventListener("mouseover",()=>{
+    fb.src="logos/fb.png"
+})
+fb.addEventListener("mouseout",()=>{
+    fb.src="logos/fbblack.png"
+})
